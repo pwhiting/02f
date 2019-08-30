5903520babfc517056aceb679c90c02d4d8849bd
whitingpt <pete@whitings.org>
2019-08-29 20:19:35 -0600

    continue

--- Forge.pm
+++ Forge.pm
@@ -2,56 +2,36 @@ package Forge;
 # this package creates forgerock specific JSON elements
 use Data::Dumper::Concise;
 use CamoMap;
-
-
-# intput: reference to a policy
-# output: array of attributes in that policy
-sub Attributes {
-  my $policy=shift;
-  my $profile=$policy->{authorization}->{headers}->{success}->{"profile-att"};
-  map {$profile->{$_}->{attribute}} (keys $profile);
-}
-
-# intput: reference to a policy
-# output: array of operations in that policy
-sub Operations {
-  my $policy=shift;
-  split(/[,\"]/,$policy->{operations});
-}
-
 # input: array of action actionValues
 # output: hash reference with each actionvalue set to true
 sub ActionValues {
+  my ($policy)=@_;
+  my @list=split(/[,\"]/,$policy->{operations});
+  @list=qw(HEAD DELETE POST GET OPTIONS PATCH PUT) if !@list;
   my $hash={};
-  my @list=Operations(shift);
-  foreach my $action (@list) {
-    $hash->{$action}="true";
-  }
+  map {$hash->{$_}="true"} @list;
   $hash;
 }
-
 #input: array of ResourceAttributes
 #output: array reference with each attribute properly defined
 sub ResourceAttributes {
+  my ($policy,$default)=@_;
+  my $profile=$policy->{authorization}->{headers}->{success}->{"profile-att"};
+  $profile=$default->{success}{"profile-att"} if($profile == undef);
+  my @list=map {$profile->{$_}->{attribute}} (keys $profile);
   my $array=[];
-  my @list=Attributes(shift);
-  foreach my $attr (@list) {
-    push($array,{
-      propertyName  => CamoMap::recase($attr),
+  map {push($array,{
+      PropertyName  => CamoMap::recase($_),
       type          =>"User",
-      propertyValues=>[]
-    });
-  }
+      propertyValues=>[]})} @list;
   $array;
 }
-
 #input: name and $policy
 #output: array reference with two elements, per AM standard resource naming
 sub Resources {
   my ($name,$policy)=@_;
   ["http*://$name/$policy*","http*://$name/$policy*?*"];
 }
-
 #stub for now
 sub Subject {
   my $hash={
@@ -64,7 +44,6 @@ sub Subject {
   };
   $hash;
 }
-
 #stub for now
 sub Condition {
     my $hash={
@@ -82,7 +61,6 @@ sub Condition {
   };
   $hash;
 }
-
 # input: three arguments - env, url, and policy name
 # output: replaces all / in url with ! and returns above variables
 #         concatinated with ! between each. If last variable is
